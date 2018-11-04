bdabe43f1a3e072a8f2fbeb7cd8c9668b64db237
btandersen383 <btandersen@utexas.edu>
2018-11-04 13:36:04 -0600

    Got algo mostly working. gives some errors though

--- fords.cpp
+++ fords.cpp
@@ -2,31 +2,60 @@
 // The program is for adjacency matrix representation of the graph 
 
 #include <stdio.h> 
+#include <stdlib.h>
+#include <iostream>
 #include <limits.h> 
+#include <malloc.h>
 #include <omp.h>
 
-void minDistance(int *dist, int *used, int *min_index) {
-    int min = INT_MAX; 
-   
-   for (int v = 0; v < V; v++)
-     if (used[v] == false && dist[v] <= min) 
-         min = dist[v], *min_index = v;
-}
+#define DEBUG 1
+
+using namespace std;
 
 void ford(int** incidence_matrix, int n, int src) {
-    // Initialize nxn matrix for distance from each node to each other node
-    // Dist to itself is 0, else is INT_MAX
     int* dist = (int*) malloc(sizeof(int)*n);
-    dist[0] = 0;
-    for(int i = 1; i < n; i++) {
-        dist[i] = INT_MAX;
+    for(int v = 0; v < n; v++) {
+        dist[v] = INT_MAX;
+    }
+    dist[src] = 0;
+
+    for(int v = 0; v < n; v++) {
+        for (int e = 0; e < n; e++) { 
+            if(incidence_matrix[v][e]) {
+                int tempDistance = dist[v] + incidence_matrix[v][e];
+                if(tempDistance < dist[e]) {
+                    dist[e] = tempDistance;
+                }
+            }
+        }
     }
 
-    // Find distance for each pair of vertecies
-    for(int i = 0; i < n; i ++) {
-        
+    for(int i = 0; i < n; i++) {
+        cout << dist[i] << endl;
     }
+
+    free(dist);
 }
+
+/* Sudo code used
+function bellmanFord(G, S)
+  for each vertex V in G
+      distance[V] <- infinite
+      previous[V] <- NULL
+  distance[S] <- 0
+  for each vertex V in G        
+    for each edge (U,V) in G
+      tempDistance <- distance[U] + edge_weight(U, V)
+      if tempDistance < distance[V]
+         distance[V] <- tempDistance
+         previous[V] <- U
+
+  for each edge (U,V) in G
+    If distance[U] + edge_weight(U, V) < distance[V}
+      Error: Negative Cycle Exists
+
+  return distance[], previous[]
+  */
 
 int** read_file(char *file_name, int *vertices) {
   FILE *f;
@@ -61,13 +90,13 @@ int main(int argc, char *argv[]) {
 
     if(argc != 3) {
         printf("Incorrect usage\n"); // sanity check
-        printf("Correct usage: ./dijktra path_to_file source_vertex");
+        printf("Correct usage: ./ford path_to_file source_vertex");
         exit(1);
     }
     
     incidence_matrix = read_file(argv[1], &vertices);
 
-    ford(incidence_matrix, vertices, argv[2]);
+    ford(incidence_matrix, vertices, atoi(argv[2]));
 
     free(incidence_matrix);
 
