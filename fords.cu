4d20c9aa1e973394d7353657ab490d5b4ecf924b
ashwin <ashwin.krishnan@utexas.edu>
2018-11-07 14:22:46 -0600

    fords works!

--- fords.cu
+++ fords.cu
@@ -5,90 +5,91 @@
 #include <malloc.h>
 #include <omp.h>
 
-__global__ void fords_kernel(int n, int u, int *mat, int *dist) {
-  int index = blockDim.x * blockIdx.x + threadIdx.x;
-  int stride = blockDim.x * gridDim.x;
+__global__ void fords_kernel(int n, uint *mat, uint *dist) {
+	int index = blockDim.x * blockIdx.x + threadIdx.x;
+	int stride = blockDim.x * gridDim.x;
 
-  if(index < n) {
-    for(int v = index; v < n; v += stride) {
-      int weight = mat[u * n + v];
-      if(weight) {
-        int temp = dist[u] + weight;
-        if(temp < dist[v]) {
-          dist[v] = temp;
-        }
-      }
-    }
-  }
+	for(int u = 0 ; u < n ; u ++){
+		for(int v = index; v < n; v+= stride){
+			int weight = mat[u * n + v];
+			if(weight){
+				int new_dist = dist[u] + weight;
+				if(new_dist < dist[v]){
+					dist[v] = new_dist;
+				}
+			}
+		}
+	}
 }
 
-void printResults(int dist[], int n, int source) { 
-  printf("Vertex\t\tDistance from Source Vertex %d\n", source);
-  for (int i = 0; i < n; i++)
-    printf("%d\t\t%d\n", i, dist[i]);
-  printf("\n\n");
+void printResults(uint dist[], int n, int source) { 
+	printf("Vertex\t\tDistance from Source Vertex %d\n", source);
+	for (int i = 0; i < n; i++)
+		printf("%d\t\t%d\n", i, dist[i]);
+	printf("\n\n");
 } 
 
 void fords(int *incidence_matrix, int n, int src) {
-  int *h_dist;
-  int *d_mat, *d_dist;
+	uint *h_dist;
+	uint *d_mat, *d_dist;
 
-  h_dist = (int*) malloc(sizeof(int) * n);
-  cudaMalloc((void**) &d_mat, sizeof(int) * n *n);
-  cudaMalloc((void**) &d_dist, sizeof(int) * n);
+	h_dist = (uint*) malloc(sizeof(uint) * n);
+	cudaMalloc((void**) &d_mat, sizeof(uint) * n *n);
+	cudaMalloc((void**) &d_dist, sizeof(uint) * n);
 
-  #pragma omp parallel for
-  for(int i = 0 ; i < n; i ++){
-    h_dist[i] = INT_MAX;
-  }
+	#pragma omp parallel for
+	for(int i = 0 ; i < n; i++){
+		h_dist[i] = INT_MAX;
+	}
 
-  h_dist[src] = 0;
+	h_dist[src] = 0;
 
-  cudaMemcpy(d_mat, incidence_matrix, sizeof(int) * n * n, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_dist, h_dist, sizeof(int) * n, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_mat, incidence_matrix, sizeof(int) * n * n, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_dist, h_dist, sizeof(int) * n, cudaMemcpyHostToDevice);
 
-  dim3 THREAD_SIZE = 256; // can't use variable size so everything is hard-coded
-  dim3 BLOCK_SIZE = 256;
+	dim3 THREAD_SIZE = 256; // can't use variable size so everything is hard-coded
+	dim3 BLOCK_SIZE = 256;
 
-  for(int iii = 0; iii < n; iii++) {
-    fords_kernel<<< BLOCK_SIZE, THREAD_SIZE >>>(n, iii, d_mat, d_dist);
-  }
-  
-  cudaMemcpy(h_dist, d_dist, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	for(int iii = 0; iii < n; iii++) {
+		// printf("Starting iteration %d of %d\n", iii + 1, n);
+		fords_kernel<<< BLOCK_SIZE, THREAD_SIZE >>>(n, d_mat, d_dist);
+	}
 
-  printResults(h_dist, n, src);
+	cudaMemcpy(h_dist, d_dist, sizeof(uint) * n, cudaMemcpyDeviceToHost);
 
-  cudaFree(d_mat);
-  cudaFree(d_dist);
+	printResults(h_dist, n, src);
+
+	cudaFree(d_mat);
+	cudaFree(d_dist);
 }
 
 int* read_file(char *file_name, int *vertices) {
-  FILE *f;
-  int *incidence_matrix;
-  int v;
+	FILE *f;
+	int *incidence_matrix;
+	int v;
 
-  f = fopen(file_name, "r");
-  if(f == NULL) {
-    printf("Error reading file\n");
-    exit(1);
-  }
+	f = fopen(file_name, "r");
+	if(f == NULL) {
+		printf("Error reading file\n");
+		exit(1);
+	}
 
-  fscanf(f, "%d\n", vertices);
-  
-  v = *vertices; // this is meant for readability later, not any optimizations
-  incidence_matrix = (int*) malloc(sizeof(int) * v * v);
+	fscanf(f, "%d\n", vertices);
 
-  for(int iii = 0; iii < v; iii++) {
-    for(int jjj = 0; jjj < v; jjj++) {
-      fscanf(f, "%d", &incidence_matrix[iii * v + jjj]);
-      if(incidence_matrix[iii* v + jjj] < 0) {
-        printf("Error: negative edge weight found at %d, %d\n", iii, jjj);
-        exit(-1);
-      }
-    }
-  }
+	v = *vertices; // this is meant for readability later, not any optimizations
+	incidence_matrix = (int*) malloc(sizeof(int) * v * v);
 
-  return incidence_matrix;
+	for(int iii = 0; iii < v; iii++) {
+		for(int jjj = 0; jjj < v; jjj++) {
+			fscanf(f, "%d", &incidence_matrix[iii * v + jjj]);
+			if(incidence_matrix[iii* v + jjj] < 0) {
+				printf("Error: negative edge weight found at %d, %d\n", iii, jjj);
+				exit(-1);
+			}
+		}
+	}
+
+	return incidence_matrix;
 }
 
 int main(int argc, char *argv[]) {
@@ -104,7 +105,7 @@ int main(int argc, char *argv[]) {
     incidence_matrix = read_file(argv[1], &vertices);
 
     for(int iii = 0; iii < vertices; iii++)
-      fords(incidence_matrix, vertices, iii);
+    	fords(incidence_matrix, vertices, iii);
 
     free(incidence_matrix);
 
