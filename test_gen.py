42f96cf501ae47aa96c2dd7c3d4611f9fb7ff0d3
ashwin <ashwin.krishnan@utexas.edu>
2018-11-20 13:19:56 -0600

    test generator can now generate weights within a given interval (0 to user provided input)

--- test_gen.py
+++ test_gen.py
@@ -3,8 +3,9 @@
 import sys
 from random import randint
 
-if(len(sys.argv) != 2):
-	print "Need number of nodes"
+if(len(sys.argv) != 3):
+	print "Need number of nodes and maximum weight"
+	print "Sample usage: ./test_gen.py 10 10"
 	exit()
 
 G = [[-1 for x in range(int(sys.argv[1]))] for y in range(int(sys.argv[1]))]
@@ -17,7 +18,7 @@ for i in range(int(sys.argv[1])):
 			if(i == j):
 				G[i][j] = 0
 			else:
-				G[i][j] = randint(0, 9)
+				G[i][j] = randint(0, int(sys.argv[2]))
 f = open("gen_test.txt", "w+")
 f.write(str(int(sys.argv[1])) + "\n")
 for i in range(int(sys.argv[1])):
